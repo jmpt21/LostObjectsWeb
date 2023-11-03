79df91c94538754137cbbb45672756bae9792dd0
JoseMPT <s20030132@itsch.edu.mx>
2023-11-03 01:14:39 -0600

    Initial configuration. All right.

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZFm262u2IhkcgpPEw-Tp4JV0UqmSDfhg",
+  authDomain: "lost-objects-app.firebaseapp.com",
+  projectId: "lost-objects-app",
+  storageBucket: "lost-objects-app.appspot.com",
+  messagingSenderId: "506654347126",
+  appId: "1:506654347126:web:5c800782d346d228983457"
+};
+
+// Initialize Firebase and products
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
