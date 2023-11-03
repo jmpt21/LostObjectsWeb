ab786a45b31163d15745ef16702abf16eadfc220
JoseMPT <s20030132@itsch.edu.mx>
2023-11-03 17:35:37 -0600

    Nav and Footer added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'LostObjectsWeb';
+  title: string = 'LOST OBJECTS WEB';
+  logoTecNM: string = 'assets/logos/LogoTecnm1.png'
+  logoITSCH: string = 'assets/logos/LogoTecHidalgo.png'
 }
